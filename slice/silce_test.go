ad94c0a124c6196aa5d7f3bae93d8f45a1757d47
huchao <2557523039@qq.com>
2022-11-25 16:46:02 +0800

    feat:add 结构体切片去重

--- slice/silce_test.go
+++ slice/silce_test.go
@@ -1,6 +1,16 @@
-/**
-    @author:Hasee
-    @data:2022/11/25
-    @note:
-**/
 package slice
+
+import (
+	"fmt"
+	"testing"
+)
+
+// 输出两个slice切片的差集
+func TestDifferenceSet(t *testing.T) {
+	leyangjun1 := []int{1, 3, 5, 6}
+	leyangjun2 := []int{1, 3, 5}
+
+	retDiff := DifferenceSet(leyangjun1, leyangjun2)
+	fmt.Println(retDiff)
+
+}
--- slice/slice.go
+++ slice/slice.go
@@ -1,6 +1,35 @@
-/**
-    @author:Hasee
-    @data:2022/11/25
-    @note:
-**/
 package slice
+
+// DifferenceSet 返回两个slice切片的差集
+func DifferenceSet(a []int, b []int) []int {
+	var c []int
+	temp := map[int]struct{}{}
+
+	for _, val := range b {
+		if _, ok := temp[val]; !ok {
+			temp[val] = struct{}{}
+		}
+	}
+
+	for _, val := range a {
+		if _, ok := temp[val]; !ok {
+			c = append(c, val)
+		}
+	}
+
+	return c
+}
+
+// RemoveRepByMap 通过map主键唯一的特性过滤重复元素
+// 结构体切片去重
+func removeRepByMap(slc []*SeriesRes) []*SeriesRes {
+	resultMap := make(map[string]*SeriesRes, len(slc))
+	for _, v := range slc {
+		resultMap[v.SeriesId] = v
+	}
+	var result []*SeriesRes
+	for _, v := range resultMap {
+		result = append(result, v)
+	}
+	return result
+}
