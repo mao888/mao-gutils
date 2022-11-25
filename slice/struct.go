1e74cd91a1fe47f3dc8c612428dd22ea1027473b
huchao <2557523039@qq.com>
2022-11-25 16:48:06 +0800

    feat:add 结构体切片去重

--- slice/struct.go
+++ slice/struct.go
@@ -9,17 +9,3 @@ type SeriesRes struct {
 	DnaCounts         int64   `json:"dnaCounts"`         // 累计发行数量/dna数字资产数
 	DnaPrice          float64 `json:"dnaPrice"`          // 发行金额
 }
-
-// RemoveRepByMap 通过map主键唯一的特性过滤重复元素
-// 结构体切片去重
-func removeStructRepByMap(slc []*SeriesRes) []*SeriesRes {
-	resultMap := make(map[string]*SeriesRes, len(slc))
-	for _, v := range slc {
-		resultMap[v.SeriesId] = v
-	}
-	var result []*SeriesRes
-	for _, v := range resultMap {
-		result = append(result, v)
-	}
-	return result
-}
