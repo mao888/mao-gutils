4cd332e0a9be5f9ae8e8489d8d8e46e40b3c05a2
huchao <2557523039@qq.com>
2023-01-06 22:33:33 +0800

    feat:add number

--- constants/constants.go
+++ constants/constants.go
@@ -60,3 +60,16 @@ var SendManageEnvType = map[int32]string{
 	1: "prod",
 	2: "test",
 }
+
+const (
+	NumberZero  = 0
+	NumberOne   = 1
+	NumberTwo   = 2
+	NumberThree = 3
+	NumberFour  = 4
+	NumberFive  = 5
+	NumberSix   = 6
+	NumberSeven = 7
+	NumberEight = 8
+	NumberNine  = 9
+)
